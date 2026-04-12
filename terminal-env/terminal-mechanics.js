a5076972d41ffe9548ad002b11154a76b3f0e958
Somer Le <thaosle4@gmail.com>
2026-04-12 01:23:25 -0400

    adding the thingsg

--- terminal-env/terminal-mechanics.js
+++ terminal-env/terminal-mechanics.js
@@ -290,34 +290,98 @@ function addComment() {
   // Get user comment + add it to the chat screen
   let userCommandDiv = document.querySelector(".user-command");
   let userCommand = document.querySelector(".user-command").value.trim();
-  // let directory = document.querySelector(".directory");
-  console.log("adding comment!");
+  let directory = document.querySelector(".directory");
   if (userCommand === "") {
-    commentsDiv.innerHTML += `<label class="green"><span class="yellow">unix</span>@user:~$ <span class="white">${userCommand}</span> </label>`;
+    commentsDiv.innerHTML += `<div>Please enter a command.</div>`;
+    return;
   }
   pastCommands.push(userCommand);
-
-  newComment.innerHTML = `<label class="green"><span class="yellow">unix</span>@user:~$ <span class="white">${userCommand}</span> </label>`;
+  newComment.innerText = `> ${userCommand}`;
   commentsDiv.appendChild(newComment);
-  let commandParts = userCommand.split(" ");
-  //console.log(commandParts[0] + " --> status: " + mainCmds.includes(commandParts[0]));
+
   if (allCmds.includes(userCommand)) {
-    commentsDiv.innerHTML += rootCmds[userCommand]();
-    console.log("found in the command map!");
-  } else if (mainCmds.includes(commandParts[0])) {
-    overallOutcome = commandParts;
-    rootCmds[commandParts[0]]();
+    if (userCommand == "clear") {
+      commentsDiv.innerHTML = "";
+    } else if (userCommand == "help") {
+      let currentDirArray =
+        currentlyIn === "root"
+          ? [...Object.keys(rootCmds), ...mainCmds]
+          : [...themes, ...mainCmds];
+      commentsDiv.innerHTML += `<ul> ${currentDirArray
+        .map((command) => {
+          return `<li class="ls-item">${command}</li>`;
+        })
+        .join("")} </ul>`;
+    } else if (userCommand == "ls") {
+      if (currentlyIn === "themes") {
+        commentsDiv.innerHTML += `<div class="ls-cont"> ${[...themes, "root"]
+          .map((theme) => {
+            return `<div class="ls-item">${theme}</div>`;
+          })
+          .join("")} </div>`;
+      } else {
+        commentsDiv.innerHTML += `<div class="ls-cont"> ${Object.keys(rootCmds)
+          .map((command) => {
+            return `<div class="ls-item">${command}</div>`;
+          })
+          .join("")} </div>`;
+      }
+    } else if (userCommand == "cd ..") {
+      if (currentlyIn == "root") {
+        commentsDiv.innerHTML += `<div>Already in the root directory.</div>`;
+        userCommandDiv.value = "";
+        return;
+      }
+      currentlyIn = "root";
+      let directories = directory.innerText.split("/");
+      directories.pop();
+      directory.innerHTML = "";
+      directory.innerHTML = directories
+        .map((x) => {
+          if (x !== "") return `/${x}`;
+        })
+        .join("");
+    } else if (
+      (currentlyIn === "root" && Object.keys(rootCmds).includes(userCommand)) ||
+      themes.includes(userCommand)
+    ) {
+      if (userCommand.startsWith("get ")) {
+        window.open(rootCmds[userCommand]);
+        return;
+      } else if (
+        userCommand === "experience" ||
+        userCommand === "certificates" ||
+        userCommand === "education"
+      ) {
+        commentsDiv.innerHTML += `<ul>${rootCmds[userCommand]
+          .map((x) => {
+            return `<li>${x}</li>`;
+          })
+          .join("")}</ul>`;
+        userCommandDiv.value = "";
+        return;
+      }
+      if (userCommand === "cd themes") {
+        currentlyIn = "themes";
+        directory.innerHTML += "/themes";
+        commentsDiv.innerHTML += `<ul>${themes
+          .map((theme) => {
+            return `<li>${theme}</li>`;
+          })
+          .join("")}</ul>`;
+      } else if (currentlyIn === "themes" && themes.includes(userCommand)) {
+        changeTheme(userCommand);
+      } else {
+        commentsDiv.innerHTML += rootCmds[userCommand];
+      }
+    } else if (
+      currentlyIn === "themes" &&
+      Object.keys(rootCmds).includes(userCommand)
+    ) {
+      handleInvalidCommand(userCommand);
+    }
   } else {
-    // if (allCmds.map(cmd => `${cmd.includes(`cd `)}`).includes("true"))
-    // {
-    //     commentsDiv.innerHTML += `<div class="white"> ${userCommand.substring(4, userCommand.length)} </div>`;
-    //     console.log("something is here I don't know what!" + userCommand.substring(4, userCommand.length));
-    // }
-    // else
-    // {
     handleInvalidCommand(userCommand);
-    console.log("what is this!!");
-    // }
   }
   userCommandDiv.value = "";
 }
