15fa9db3b8272635ca62330f45729278c1823073
el5081 <el5081@g.rit.edu>
2026-04-24 13:47:13 -0400

    fixed more navigation

--- assets/files/nav.php
+++ assets/files/nav.php
@@ -4,27 +4,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Intro To UNIX</title>
-    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="../assets/css/styles.css">
 </head>
 <body>
     <!-- Header Section -->
     <section class="grid_container">
-    <img id="mobile_nav" src="/assets/images/Nav Bars.png" alt="Mobile Navigation Button">
-    <a href="/pages/index.php">
-        <img id="title_pc" src="/assets/images/Long Title.png" alt="Intro to UNIX title card">
-        <img id="title_mobile" src="/assets/images/Logo.png" alt="Intro to UNIX title card">
+    <img id="mobile_nav" src="../assets/images/Nav Bars.png" alt="Mobile Navigation Button">
+    <a href="index.php">
+        <img id="title_pc" src="../assets/images/Long Title.png" alt="Intro to UNIX title card">
+        <img id="title_mobile" src="../assets/images/Logo.png" alt="Intro to UNIX title card">
     </a>
     <nav>
         <ul class="navbar">
-            <li><a href="/pages/index.php">Home</a></li>
-            <li><a href="/pages/history.php">History</a></li>
-            <li><a href="/pages/tutorials.php">Tutorial</a></li>
-            <li><a href="/pages/quiz.php">Quiz</a></li>
-            <li><a href="/pages/scoreboard.php">Scoreboard</a></li>
-            <li><a href="/pages/grading.php">Grading</a></li>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="history.php">History</a></li>
+            <li><a href="tutorials.php">Tutorial</a></li>
+            <li><a href="quiz.php">Quiz</a></li>
+            <li><a href="scoreboard.php">Scoreboard</a></li>
+            <li><a href="grading.php">Grading</a></li>
         </ul>
         <ul class="navbar-2">
-            <li><a href="/pages/login.php">Login</a></li>
+            <li><a href="login.php">Login</a></li>
         </ul>
     </nav>
     <!-- Header Section ^ -->
