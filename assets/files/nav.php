89b10c8c760e92681c43e2ad6228ddcbd33c1ab3
unknown <all9629@rit.edu>
2026-05-03 01:33:35 -0400

    finished the side bar changes

--- assets/files/nav.php
+++ assets/files/nav.php
@@ -9,26 +9,33 @@
 <body>
     <!-- Header Section -->
     <section class="grid_container">
-    <img id="mobile_nav" src="../assets/images/Nav_Bars.png" alt="Mobile Navigation Button">
-    <a href="index.php">
-        <img id="title_pc" src="../assets/images/Long_Title.png" alt="Intro to UNIX title card">
-        <img id="title_mobile" src="../assets/images/Mobile_Logo.png" alt="Intro to UNIX title card">
-    </a>
-    <nav>
-        <ul class="navbar">
-            <li><a href="index.php">Home</a></li>
-            <li><a href="history.php">History</a></li>
-            <li><a href="tutorials.php">Tutorial</a></li>
-            <li><a href="quiz.php">Quiz</a></li>
-            <li><a href="scoreboard.php">Scoreboard</a></li>
-            <li><a href="grading.php">Grading</a></li>
-        </ul>
-        <ul class="navbar-2">
-            <?php if (isset($_SESSION['username'])): ?>
-            <li><a href="logout.php">Logout</a></li>
-            <?php else: ?>
-            <li><a href="login.php">Login</a></li>
-            <?php endif; ?>
-        </ul>
-    </nav>
+        <img id="mobile_nav" src="../assets/images/Nav_Bars.png" alt="Mobile Navigation Button">
+        <a href="index.php">
+            <img id="title_pc" src="../assets/images/Long_Title.png" alt="Intro to UNIX title card">
+            <img id="title_mobile" src="../assets/images/Mobile_Logo.png" alt="Intro to UNIX title card">
+        </a>
+        <nav>
+            <ul class="navbar">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="history.php">History</a></li>
+                <li><a href="tutorials.php">Tutorial</a></li>
+                <li><a href="quiz.php">Quiz</a></li>
+                <li><a href="scoreboard.php">Scoreboard</a></li>
+                <li><a href="grading.php">Grading</a></li>
+            </ul>
+            <ul class="navbar-2">
+                <?php if (isset($_SESSION['username'])): ?>
+                <li><a href="logout.php">Logout</a></li>
+                <?php else: ?>
+                <li><a href="login.php">Login</a></li>
+                <?php endif; ?>
+            </ul>
+        </nav>
+    </section>
+
+    <script>
+        document.getElementById('mobile_nav').addEventListener('click', function() {
+            document.querySelector('nav').classList.toggle('open');
+        });
+    </script>
     <!-- Header Section ^ -->
