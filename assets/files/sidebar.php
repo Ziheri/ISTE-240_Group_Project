efdfd064a98962584beca70f2e7c75ec8776d5be
Xander113005 <all9629@rit.edu>
2026-05-03 15:08:18 -0400

    fixinfg th side bar

--- assets/files/sidebar.php
+++ assets/files/sidebar.php
@@ -1,11 +1,11 @@
 <ul class="sidebar">
-    <li class="sidebar_text">&gt; <a href="#requirements">Requirements & Setup</a></li>
+    <li class="sidebar_text">&gt <a href="#requirements">Requirements & Setup</a></li>
     <li class="sidebar_inbetween">--------------------</li>
-    <li class="sidebar_text">&gt; <a href="#file-system">File System Hierarchy</a></li>
+    <li class="sidebar_text">&gt <a href="#file-system">File System Hierarchy</a></li>
     <li class="sidebar_inbetween">--------------------</li>
-    <li class="sidebar_text">&gt; <a href="#security">Security & Permissions</a></li>
+    <li class="sidebar_text">&gt <a href="#security">Security & Permissions</a></li>
     <li class="sidebar_inbetween">--------------------</li>
-    <li class="sidebar_text">&gt; <a href="#shell">Shell & Sub Support</a></li>
+    <li class="sidebar_text">&gt <a href="#shell">Shell & Sub Support</a></li>
     <li class="sidebar_inbetween">--------------------</li>
-    <li class="sidebar_text">&gt; <a href="#scripting">Scripting</a></li>
+    <li class="sidebar_text">&gt <a href="#scripting">Scripting</a></li>
 </ul>
