c92e9ba7eaaecbbbf893b4fc099eaa5c3a58e629
Sommer <thaosle4@gmail.com>
2026-05-03 22:14:06 -0400

    question checks with the terminal

---
+++ terminal-env/quizQuestinoValidation.js
@@ -0,0 +1,45 @@
+function q1() {
+  //
+}
+function q2() {
+  //
+}
+function q3() {
+  //
+}
+function q4() {
+  //
+}
+function q5() {
+  //
+}
+function q6() {
+  //
+}
+function q7() {
+  //
+}
+function q8() {
+  //
+}
+function q9() {
+  //
+}
+function q10() {
+  //
+}
+function q11() {
+  //
+}
+function q12() {
+  //
+}
+function q13() {
+  //
+}
+function q14() {
+  //
+}
+function q15() {
+  //
+}
